32c570acd644cd459bd6df2795dcd7c02d9d2155
Reinis Riekstins <riekstins.reinis@gmail.com>
2017-03-17 14:19:05 +0200

    added parallax effect to logo, back and forebird

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-import './styles/index.sass';
-import $ from 'jquery';
+import './styles/index.sass'
+import $ from 'jquery'
 
-$;
+const
+  $window = $(window),
+  $logo = $('.logo'),
+  $backbird = $('.back-bird'),
+  $forebird = $('.fore-bird')
+
+$window.scroll(() => {
+  var wScroll = $window.scrollTop()
+  console.log(wScroll)
+
+  $logo.css({
+    transform: `translate(0px, ${wScroll/2}%)`
+  })
+
+  $backbird.css({
+    transform: `translate(0px, ${wScroll/4}%)`
+  })
+
+  $forebird.css({
+    transform: `translate(0px, -${wScroll/50}%)`
+  })
+})
